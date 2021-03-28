aa9fd58c0c7c2b34e9eca4a5a7796ebf0848c011
deinesegel <81515034+deinesegel@users.noreply.github.com>
2021-03-28 21:26:53 +0800

    Update Program.cs

--- 作业20210315/Program.cs
+++ 作业20210315/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,13 +9,35 @@ namespace 作业20210315
     {
         static void Main(string[] args)
         {
+            int i;
+            string x1,y1;
             int x, y;
             string z;
+            z = Convert.ToString(Console.ReadLine());
+            if(z=="+"||z=="-")
+            {
+                Console.WriteLine("请选择：1.正常加减法，2.重构方法");
+                i = Convert.ToInt32(Console.ReadLine());
+                if (i == 1) { 
+                    x = Convert.ToInt32(Console.ReadLine());
+                    y = Convert.ToInt32(Console.ReadLine()); 
+                    Counter.counter(x,y,z);
+                    Counter.judge(x, y);
+                }
+                if (i == 2)
+                {
+                    x1 = Convert.ToString(Console.ReadLine());
+                    y1 = Convert.ToString(Console.ReadLine());
+                    Counter.counter1(x1, y1, z);
+                }
+            }
             x = Convert.ToInt32(Console.ReadLine());
             y = Convert.ToInt32(Console.ReadLine());
-            z = Convert.ToString(Console.ReadLine());
-            Counter.counter(x,y,z);
+            Counter.counter(x, y, z);
             Counter.judge(x, y);
+            
+          
+            
             Console.ReadKey();
         }
         
